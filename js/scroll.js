6188268b1eecb0771273a9e159d3150ac9d1ef45
Vevina Trinh <vevinatrinh@gmail.com>
2017-12-23 23:16:59 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,26 @@
+var scroller = {};
+scroller.e = document.getElementById("main");
+
+if (scroller.e.addEventListener) {
+scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
+scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+} else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
+
+function MouseWheelHandler(e) {
+
+// cross-browser wheel delta
+var e = window.event || e;
+var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
+
+var pst = $('#main').scrollLeft() + delta;
+
+if (pst < 0) {
+    pst = 0;
+} else if (pst > $('mainContainer').width()) {
+    pst = $('mainContainer').width();
+}
+
+$('#main').scrollLeft(pst);
+
+return false;
+}
