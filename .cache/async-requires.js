7b2257c488b0c440d803a9b5e224f410d57a55f8
Vevina Trinh <vevinatrinh@gmail.com>
2019-07-21 03:06:01 -0400

    fixed untracked files

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/vevinatrinh/Documents/repos/partialfractions.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("/Users/vevinatrinh/Documents/repos/partialfractions.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-about-js": () => import("/Users/vevinatrinh/Documents/repos/partialfractions.github.io/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */)
-}
-
